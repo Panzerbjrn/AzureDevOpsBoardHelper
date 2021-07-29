4b85041cffd9712b9a234eac31e8d4ff4170b745
Lars Panzerbjørn <lars@panzerbjrn.eu>
2021-07-29 13:18:41 +0200

    Test

---
+++ Functions/Connect-AzDoItems.ps1
@@ -0,0 +1,103 @@
+﻿Function Connect-AzDoItems
+{
+<#
+	.SYNOPSIS
+		Links two Azure Devops item in a parent/child relationship
+
+	.DESCRIPTION
+		Links two Azure Devops item in a parent/child relationship
+
+	.EXAMPLE
+
+
+	.PARAMETER PersonalAccessToken
+		This is your personal access token from Azure Devops.
+
+	.PARAMETER OrganizationName
+		The name of your Azure Devops Organisation
+
+	.PARAMETER ProjectName
+		The name of your Azure Devops Project or Team
+
+	.PARAMETER ParentItemID
+		The name of your Azure Devops Project or Team
+
+	.PARAMETER ChildItemID
+		The name of your Azure Devops Project or Team
+
+	.INPUTS
+		Input is from command line or called from a script.
+
+	.OUTPUTS
+		This will output the logfile.
+
+	.NOTES
+		Version:			0.1
+		Author:				Lars Panzerbjørn
+		Creation Date:		2020.07.31
+		Purpose/Change: Initial script development
+#>
+	[CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact='Low')]
+	param
+	(
+		[Parameter(Mandatory)]
+		[Alias('PAT')]
+		[string]$PersonalAccessToken,
+
+		[Parameter(Mandatory)]
+		[Alias('Company')]
+		[string]$Organisation,
+
+		[Parameter(Mandatory)]
+		[Alias('TeamName')]
+		[string]$Project,
+
+		[Parameter(Mandatory)][string]$ParentItemID,
+		[Parameter(Mandatory)][string]$ChildItemID
+	)
+
+	BEGIN
+	{
+		Write-Verbose "Beginning $($MyInvocation.Mycommand)"
+		$JsonContentType = 'application/json-patch+json'
+		$BaseUri = "https://dev.azure.com/$($Organisation)/"
+		$Uri = $BaseUri + "$Project/_apis/wit/workitems/$ChildItemID`?api-version=6.1-preview.3"
+
+
+		$Token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($PersonalAccessToken)"))
+		$Header = @{Authorization = 'Basic ' + $Token;accept=$JsonContentType}
+	}
+
+	PROCESS
+	{
+		Write-Verbose "Processing $($MyInvocation.Mycommand)"
+		$Value = @{
+			rel = "System.LinkTypes.Hierarchy-Reverse"
+			url = "https://dev.azure.com/$Organisation/$Project/_apis/wit/workItems/$ParentItemID"
+			}
+		$Attributes = @{
+			isLocked = $False
+			name = "Parent"
+		}
+
+
+		$Body = @([pscustomobject]@{
+				op = "add"
+				path = '/relations/-'
+				value = $Value
+				Attributes = $Attributes
+			}
+		)
+
+		$Body = ConvertTo-Json $Body
+		$Body
+		$Result = Invoke-RestMethod -Uri $uri -Method PATCH -Headers $Header -ContentType $JsonContentType -Body $Body
+
+	}
+	END
+	{
+		Write-Verbose "Ending $($MyInvocation.Mycommand)"
+		#$Body
+		$Result
+	}
+}
---
+++ Functions/Remove-AzDoUserStoryWorkItem.ps1
@@ -0,0 +1,84 @@
+﻿Function Remove-AzDoUserStoryWorkItem
+{
+<#
+	.SYNOPSIS
+		This will delete a work item.
+
+	.DESCRIPTION
+		This will delete a work item. The item is moved to the recycle binn in case it needs to be restored.
+		The REST API has an option to delete fully, without being able to restore from recycle bin, but that is not implemented here.
+
+	.EXAMPLE
+		Remove-AzDoUserStoryWorkItem -PersonalAccessToken 'gh5553hiih5lfewahq7n3g7x7oieuothushimanuoch8szn3u2sq' -Organisation "panzerbjrn" -Project "Alpha Devs" -WorkItemID 60505
+
+	.EXAMPLE
+		$PAT = 'gh5553hiih5lfewahq7n3g7x7oieuothushimanuoch8szn3u2sq'
+		$Organisation = "panzerbjrn"
+		$TeamName = "Alpha Devs"
+		Remove-AzDoUserStoryWorkItem -PersonalAccessToken $PAT -Organisation $Organisation -Project $TeamName -WorkItemID 60505
+
+	.PARAMETER PersonalAccessToken
+		This is your personal access token from Azuree Devops.
+
+	.PARAMETER OrganisationName
+		The name of your Azure Devops Organisation
+
+	.PARAMETER Project
+		The name of your Azure Devops project. Is also often a team name.
+
+	.PARAMETER WorkItemID
+		The ID number of the work item you wish to delete
+
+	.INPUTS
+		Input is from command line or called from a script.
+
+	.OUTPUTS
+		This will output the item being deleted.
+
+	.NOTES
+		Version:			1
+		Author:				Lars Panzerbjørn
+		Creation Date:		2020.07.31
+		Purpose/Change: Initial script development
+#>
+	[CmdletBinding(SupportsShouldProcess=$True,ConfirmImpact='Low')]
+	param
+	(
+		[Parameter(Mandatory)]
+		[Alias('PAT')]
+		[string]$PersonalAccessToken,
+
+		[Parameter(Mandatory)]
+		[Alias('Company')]
+		[string]$Organisation,
+
+		[Parameter(Mandatory)]
+		[Alias('TeamName')]
+		[string]$Project,
+
+		[Parameter(Mandatory)]
+		[Alias('WorkItem','ID')]
+		[string]$WorkItemID
+
+	)
+
+	BEGIN
+	{
+		Write-Verbose "Beginning $($MyInvocation.Mycommand)"
+		$Token = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes(":$($PersonalAccessToken)"))
+		$Header = $AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + $Token;accept=$JsonContentType}
+
+		$Uri = "https://dev.azure.com/$Organisation/$Project/_apis/wit/workitems/$($workItemId)?api-version=5.1"
+	}
+
+	PROCESS
+	{
+		Write-Verbose "Processing $($MyInvocation.Mycommand)"
+		$WItem = Invoke-RestMethod -Uri $uri -Method DELETE -Headers $AzureDevOpsAuthenicationHeader	#This deletes the Work item
+	}
+	END
+	{
+		Write-Verbose "Ending $($MyInvocation.Mycommand)"
+		$WItem
+	}
+}
