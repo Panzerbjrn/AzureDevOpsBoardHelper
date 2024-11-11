97529751d7d708e2a186a954506895b97a0c89c6
Lars.Panzerbjrn <lars@panzerbjrn.eu>
2024-11-11 17:27:33 +0100

    Refreshing

---
+++ AzureDevOpsBoardHelper/Functions/Add-AzDoPipeline.ps1
@@ -0,0 +1,87 @@
+﻿Function Add-AzDoPipeline{
+<#
+	.SYNOPSIS
+		Creates a work item of the type Task
+
+	.DESCRIPTION
+		Creates a work item of the type Task
+
+	.EXAMPLE
+		New-AzDoPipeline -PersonalAccessToken $PAT -PipelineName "Test_Pipe" -Project "CI Team" -RepositoryName CoolRepo
+
+	.PARAMETER PipelineName
+		The name of pipeline you wish to create
+
+	.PARAMETER ProjectName
+		The name of your Azure Devops Project or Team
+
+	.PARAMETER RepositoryId
+		The ID of your Repository
+
+	.PARAMETER FolderPath
+		If you use folders to organise your Pipelines, this can be added here.
+
+	.PARAMETER YAMLPath
+		The path to the yaml file the pipeline should use.
+
+	.INPUTS
+		Input is from command line or called from a script.
+
+	.NOTES
+		Author:			Lars Panzerbjørn
+		Creation Date:	2024.11.09
+		Purpose/Change: Initial script development
+#>
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory)]
+		[string]$PipelineName,
+
+		[Parameter(Mandatory)]
+		[Alias('TeamName')]
+		[string]$Project,
+
+		[Parameter(Mandatory)]
+		[string]$RepositoryId,
+
+		[Parameter()][string]$FolderPath,
+
+		[Parameter(Mandatory)]
+		[string]$YAMLPath
+	)
+
+	BEGIN{
+		Write-Verbose "Beginning $($MyInvocation.Mycommand)"
+		$Uri = $BaseUri + "$Project/_apis/pipelines?api-version=7.0"
+	}
+
+	PROCESS{
+		Write-Verbose "Processing $($MyInvocation.Mycommand)"
+
+		$Body = @{
+			name = $pipelineName
+			configuration = @{
+				type = "yaml"
+				path = $yamlPath
+				repository = @{
+					id = $repositoryId
+					name = $repositoryId
+					type = "azureReposGit"
+				}
+			}
+		}
+		$Body
+
+		IF($FolderPath){
+			$Body.FolderPath = $FolderPath
+		}
+
+		$Result = Invoke-RestMethod -Uri $uri -Method POST -Headers $Header -ContentType "application/json" -Body $Body
+
+
+	}
+	END{
+		Write-Verbose "Ending $($MyInvocation.Mycommand)"
+		$Result #| Select-Onject -Property id, name, url
+	}
+}
---
+++ AzureDevOpsBoardHelper/Functions/Get-AzDORepo.ps1
@@ -0,0 +1,51 @@
+Function Get-AzDORepo{
+<#
+	.SYNOPSIS
+		This will get This will get various details for a Repo in your Organisation.
+
+	.DESCRIPTION
+		This will get This will get various details for a Repo in your Organisation.
+
+	.EXAMPLE
+		Get-AzDORepo -Project "Alpha Devs"
+
+	.PARAMETER RepoName
+		The name of your Azure Devops Repo.
+
+	.PARAMETER Project
+		The name of your Azure Devops project. Is also often a team name.
+
+	.INPUTS
+		Input is from command line or called from a script.
+
+	.OUTPUTS
+		This will output a details for the repo.
+
+	.NOTES
+		Author:				Lars Panzerbjørn
+		Creation Date:		2024.11.11
+#>
+	[CmdletBinding()]
+	param(
+		[Parameter(Mandatory)]
+		[string]$RepoName
+
+		[Parameter(Mandatory)]
+		[Alias('TeamName')]
+		[string]$Project
+	)
+
+	BEGIN{
+		Write-Verbose "Beginning $($MyInvocation.Mycommand)"
+        $Uri = $BaseUri + "$Project/_apis/git/repositories/$RepoName`?api-version=7.0"
+	}
+
+	PROCESS{
+		Write-Verbose "Processing $($MyInvocation.Mycommand)"
+		$Response = Invoke-RestMethod -Uri $Uri -Method get -Headers $Header		#Retrieves list of Pipelines
+	}
+	END{
+		Write-Verbose "Ending $($MyInvocation.Mycommand)"
+		$Response
+	}
+}
